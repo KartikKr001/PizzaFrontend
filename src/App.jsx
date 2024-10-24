f07b0137c9d8735b3bb8f103c2574f3873a706bb
Kartik Kumar <kartikkrprajapati01@gmail.com>
2024-10-24 23:16:12 +0530

    Added Login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from '../src/Components/Home'
 import { Route } from 'react-router-dom'
 import { Routes } from 'react-router-dom'
 import Signup from './Auth/Signup'
+import Login from './Auth/Login'
 
 function App() {
 
@@ -10,7 +11,8 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/auth/signup" element={<Signup />}/>
+        <Route path="/auth/signup" element={<Signup/>}/>
+        <Route path="/auth/login" element={<Login/>}/>
       </Routes>
     </>
   )
