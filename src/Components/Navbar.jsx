9134579883a47bab98b49601739f4f610ebe3271
Kartik Kumar <kartikkrprajapati01@gmail.com>
2024-10-30 09:11:48 +0530

    Added redux thunks for login and logout

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
 import PizzaLogo from '../assets/images/pizza1.png'
+import {useDispatch, useSelector}  from 'react-redux'
+import { logout } from '../Redux/Slices/AuthSlice';
+
 function Navbar(){
+
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+    const dispatch = useDispatch();
+
+    async function handleLogout(e){
+        e.preventDefault();
+        dispatch(logout());
+    }
+
+
     return (
         <nav>
             <div className="flex items-center justify-around h-16 text-[#687280] font-mono border-none shadow-md">
@@ -7,23 +21,32 @@ function Navbar(){
                     <h3>Pizza App</h3>                        
                     <img src={PizzaLogo} alt="" />
                 </div>
-                <div>
-                    <div className='hidden md:block'>
+
+                <div className='hidden md:block'>
                     <ul className='flex gap-4'>
-                        <li className='hover:text-[#Ff9110]'>
-                            {' '}
-                            Menu {' '}
-                        </li>
-                        <li className='hover:text-[#Ff9110]'>
-                            {' '}
-                            Services {' '}
-                        </li>
-                        <li className='hover:text-[#Ff9110]'>
-                            {' '}
-                            About {' '}
+                            <li className='hover:text-[#Ff9110]'>
+                                {' '}
+                                Menu {' '}
+                            </li>
+                            <li className='hover:text-[#Ff9110]'>
+                                {' '}
+                                Services {' '}
+                            </li>
+                            <li className='hover:text-[#Ff9110]'>
+                                {' '}
+                                About {' '}
+                            </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul className='flex gap-4'>
+                        <li className='hover:text-[#FF9110]'>
+                            {isLoggedIn ? (
+                                <Link onClick={handleLogout}>Logout</Link>
+                            ):
+                            (<Link to='/auth/login'> Login </Link>)}
                         </li>
                     </ul>
-                    </div>
                 </div>
             </div>
         </nav>
--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -27,11 +27,65 @@ export const createAccount = createAsyncThunk('/auth/createAccount',async (data)
     }
 })
 
+export const login = createAsyncThunk('/auth/login',async (data) =>{
+    console.log("Incoming data to thunk",data);
+    try{
+        const response = axiosInstance.post('/auth/login',data);
+        toast.promise(response ,{
+            loading : 'Hold tight, we are getting you in...',
+            success : 'Logged in successfully',
+            error : 'Ohh No! something went wrong, Please try again'
+        });
+        const apiResponse = await response;
+        return apiResponse;
+    }
+    catch(error){
+        console.log(error);
+        throw error;
+    }
+});
+
+
+export const logout = createAsyncThunk('/auth/logout',async () =>{
+    try{
+        const response = axiosInstance.post('/auth/logout');
+        toast.promise(response ,{
+            loading : 'Logging out...',
+            success : 'Logged out successfully',
+            error : 'Ohh No! something went wrong, Please try again'
+        });
+        const apiResponse = await response;
+        return apiResponse;
+    }
+    catch(error){
+        console.log(error);
+        throw error;
+    }
+});
+
+
 const AuthSlice  = createSlice({
     name : 'auth',
     initialState,
-    reducers : {
-        
+    reducers : {},
+    extraReducers  : (builder) =>{      //Redux passes this builder object by itself
+        //changes in state after successful execution of thunk
+        builder.addCase(login.fulfilled,(state,action) =>{  
+            state.isLoggedIn = true,
+            state.role = action?.payload?.data?.data?.userRole,
+            state.data = action?.payload?.data?.data?.userData
+            
+            localStorage.setItem('isLoggedIn',true)
+            localStorage.setItem('role',action?.payload?.data?.data?.userRole)
+            localStorage.setItem('data',JSON.stringify(action?.payload?.data?.data?.userData));
+        })
+
+
+        builder.addCase(logout.fulfilled,(state) =>{  
+            localStorage.clear();
+            state.isLoggedIn = false 
+        })
+
     }
 });
 
